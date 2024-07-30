decb49543e97f02a5d8eb4c3ff0fb2bea787a7e0
spageektti <git@spageektti.cc>
2024-07-30 13:23:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // general options
 const width = 125;
-const height = width; //recommended to be square, but don't have to
+const height = 125; // recommended to be square, but don't have to
 const boxes_width = 3; // how many boxes to draw
-const boxes_height = boxes_width; // must be a square to be tic tac toe game
+const boxes_height = 3; // boxes must be squares to be tic tac toe game, but you can change it if you want
 
 // fill-the-board options
 const fill = true; // whether to make your blot machine play the TicTacToe game with himself, or make the board blank
 const how_many_in_row_to_win = 3; // only needed when fill=true
 // randomness options (only needed when fill=true)
 
+const drawable_size = Math.min(width, height);
 
 setDocDimensions(width, height);
 
@@ -18,17 +19,20 @@ let board = [];
 // add vertical lines
 for (let i = 1; i < boxes_width; i++) {
   board.push([
-    [i * (width / boxes_width), 0],
-    [i * (width / boxes_width), width]
+    [i * (drawable_size / boxes_width), 0],
+    [i * (drawable_size / boxes_width), drawable_size]
   ]);
 }
 
 // add horizontal lines
 for (let i = 1; i < boxes_height; i++) {
   board.push([
-    [0, i * (height / boxes_height)],
-    [width, i * (height / boxes_height)]
+    [0, i * (drawable_size / boxes_height)],
+    [drawable_size, i * (drawable_size / boxes_height)]
   ]);
 }
+
+// center
+bt.translate(board, [width / 2, height / 2], bt.bounds(board).cc)
 
 drawLines(board);
