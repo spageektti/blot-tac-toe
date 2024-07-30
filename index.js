bb03311640c1f6f89b25601af2b73c25d87e95ea
spageektti <git@spageektti.cc>
2024-07-30 14:31:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const boxes_height = 3; // boxes must be squares to be tic tac toe game, but you
 const fill = true; // whether to make your blot machine play the TicTacToe game with himself, or make the board blank - works when the boxes are squares
 const how_many_in_row_to_win = 3; // only needed when fill=true
 // randomness options (only needed when fill=true)
-const seed = 10342234;
+const seed = 10342254;
+
+// number of control points to draw the circle (O)
+const numPoints = 8;
 
 bt.setRandSeed(seed);
 const drawable_size = Math.min(width, height);
@@ -54,12 +57,27 @@ function generate_x(x, y) {
 }
 
 function generate_o(x, y) {
-  return [bt.catmullRom([
-    [5, 0],
-    [10, 5],
-    [5, 10],
-    [0, 5]
-  ], 100)];
+  const p1 = [x * drawable_size / boxes_width, y * drawable_size / boxes_width];
+  const p2 = [x * drawable_size / boxes_width, y * drawable_size / boxes_width - drawable_size / boxes_width];
+  const p3 = [x * drawable_size / boxes_width - drawable_size / boxes_width, y * drawable_size / boxes_width - drawable_size / boxes_width];
+  const p4 = [x * drawable_size / boxes_width - drawable_size / boxes_width, y * drawable_size / boxes_width];
+
+  // center and radius of the circle
+  const centerX = (p1[0] + p3[0]) / 2;
+  const centerY = (p1[1] + p3[1]) / 2;
+  const radius = Math.min(Math.abs(p1[0] - p3[0]), Math.abs(p1[1] - p3[1])) / 2;
+
+  const controlPoints = [];
+  for (let i = 0; i < numPoints; i++) {
+    const angle = 2 * Math.PI * i / numPoints;
+    const pointX = centerX + radius * Math.cos(angle);
+    const pointY = centerY + radius * Math.sin(angle);
+    controlPoints.push([pointX, pointY]);
+  }
+
+  controlPoints.push(controlPoints[0])
+
+  return [bt.catmullRom(controlPoints, 100)];
 }
 
 if (fill) {
