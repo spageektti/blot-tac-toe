2c8936a1e809758015910ffb15abe130dc958a34
spageektti <git@spageektti.cc>
2024-07-30 14:04:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const boxes_width = 3; // how many boxes to draw
 const boxes_height = 3; // boxes must be squares to be tic tac toe game, but you can change it if you want
 
 // fill-the-board options
-const fill = true; // whether to make your blot machine play the TicTacToe game with himself, or make the board blank
+const fill = true; // whether to make your blot machine play the TicTacToe game with himself, or make the board blank - works when the boxes are squares
 const how_many_in_row_to_win = 3; // only needed when fill=true
 // randomness options (only needed when fill=true)
 const seed = 10342234;
@@ -41,5 +41,20 @@ drawLines(board);
 
 // fill the board with X and O
 if (fill) {
+  let x, y;
+  for (let i = 0; i < /*boxes_width * boxes_height*/ 1; i++) {
+    x = bt.randIntInRange(1, boxes_width);
+    y = bt.randIntInRange(1, boxes_height);
 
+    drawLines([
+      [
+        [x * drawable_size / boxes_width, y * drawable_size / boxes_width],
+        [x * drawable_size / boxes_width - drawable_size / boxes_width, y * drawable_size / boxes_width - drawable_size / boxes_width]
+      ],
+      [
+        [x * drawable_size / boxes_width, y * drawable_size / boxes_width - drawable_size / boxes_width],
+        [x * drawable_size / boxes_width - drawable_size / boxes_width, y * drawable_size / boxes_width]
+      ]
+    ])
+  }
 }
